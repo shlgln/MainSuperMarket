98b2e6e77fa62b22d1cc70c3f7f32ae68cbe4500
Soheila Golshan <s_golshan22@yahoo.com>
2021-03-14 00:58:48 -0800

    creat goodentry controller

---
+++ SuperMarket.RestApi/Controllers/GoodEntryController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperMarket.Services.GoodEntries.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperMarket.RestApi.Controllers
+{
+    [Route("api/good-entry")]
+    [ApiController]
+    public class GoodEntryController : ControllerBase
+    {
+        private readonly GoodEntryService _service;
+        public GoodEntryController(GoodEntryService service)
+        {
+            _service = service;
+        }
+
+    }
+}
