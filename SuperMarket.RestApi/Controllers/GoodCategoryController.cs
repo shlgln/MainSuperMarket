3d788b98f980a369e57571de8f201e1ce307fab5
Soheila Golshan <s_golshan22@yahoo.com>
2021-03-14 00:33:24 -0800

    add a goodcategory

---
+++ SuperMarket.RestApi/Controllers/GoodCategoryController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperMarket.Services.GoodCategories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperMarket.RestApi.Controllers
+{
+    [Route("api/good-category")]
+    [ApiController]
+    public class GoodCategoryController : ControllerBase
+    {
+        private readonly GoodCategoryService _service;
+        public GoodCategoryController(GoodCategoryService service)
+        {
+            _service = service;
+        }
+        [HttpPost]
+        public void AddGoodCategory(string title)
+        {
+            _service.AddGoodCategory(title);
+        }
+
+    }
+}
